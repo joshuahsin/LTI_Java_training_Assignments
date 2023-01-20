a3a0eb8fb3af54ea2f62ed1f5fd9f464afb2ca2b
joshuahsin <32546754+joshuahsin@users.noreply.github.com>
2023-01-20 15:46:02 +0000

    Add files via upload

---
+++ Day2Assignments/Operator.java
@@ -0,0 +1,13 @@
+package com.Day2Assignments;
+
+public class Operator {
+	public static void main(String[] args) {
+		int intVal = 100;
+		byte byteVal = (byte) intVal;
+		byte max = 127;
+		byte min = -128;
+		byte sum = (byte) (max + min);
+		
+		System.out.println("Sum = " + sum);
+	}
+}
---
+++ Day2Assignments/newEmployee.java
@@ -0,0 +1,43 @@
+package com.Day2Assignments;
+
+public class newEmployee {
+	int i;
+	int j;
+	
+	public newEmployee(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+	
+	public boolean validAge() {
+		if(i > 20 && i < 30) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean validIdNumber() {
+		if(j >= 14000 && j < 20000) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static void main(String args[]) {
+		newEmployee newEmployeeCheck = new newEmployee(22, 14000);
+		
+		boolean k = newEmployeeCheck.validAge();
+		boolean l = newEmployeeCheck.validIdNumber();
+		
+		if(k == true && l == true) {
+			System.out.print(" Is a new employee");
+		}
+		else {
+			System.out.print(" Is not a new employee");
+		}
+	}
+}
