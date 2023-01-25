6c0815b39080dd74aee911f1eb6810290b991801
joshuahsin <32546754+joshuahsin@users.noreply.github.com>
2023-01-25 22:28:37 +0000

    Add files via upload

---
+++ Day5Assignments/src/Assignment3/Example.java
@@ -0,0 +1,17 @@
+package Assignment3;
+
+public class Example implements InterOne {
+
+	@Override
+	public void methodOne() {
+		//varTwo = varTwo + 10;
+		System.out.println("Value of varTwo: " + varTwo);
+		System.out.println("Good Morning");
+	}
+
+	@Override
+	public void methodTwo() {
+		System.out.println("Good Afternoon");
+	}
+	
+}
